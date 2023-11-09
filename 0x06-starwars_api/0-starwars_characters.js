5e6336cbe0b8c656fa8d56e9be8431000d266881
MutheuMC <95241952+MutheuMC@users.noreply.github.com>
2023-11-10 01:09:14 +0300

    updated function request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,42 @@
 #!/usr/bin/node
 const request = require('request');
-const movieId = process.argv[2];
-const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
-  method: 'GET'
-};
 
-request(options, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
+// Base URL for the Star Wars API
+const baseUrl = 'https://swapi.dev/api';
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+// Function to fetch characters from a specific movie
+function fetchCharactersFromMovie(movieId) {
+  const movieUrl = `${baseUrl}/films/${movieId}/`;
+  
+  request(movieUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      const charactersUrl = movieData.characters;
+
+      console.log(`Characters in ${movieData.title}:`);
+      
+      // Fetch and print character names
+      charactersUrl.forEach((characterUrl, index) => {
+        request(characterUrl, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const characterData = JSON.parse(body);
+            console.log(`${index + 1}. ${characterData.name}`);
+          } else {
+            console.error(`Error fetching character data: ${error}`);
+          }
+        });
+      });
+    } else {
+      console.error(`Error fetching movie data: ${error}`);
     }
   });
+}
+
+// Command line argument for movie ID
+const movieId = process.argv[2];
+
+if (movieId) {
+  fetchCharactersFromMovie(movieId);
+} else {
+  console.log('Please provide a movie ID as a command line argument (e.g., node script.js 3)');
 }
