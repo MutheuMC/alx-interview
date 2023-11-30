8691332d53c272d14fcd052ed2ba4c3a88189212
MutheuMC <95241952+MutheuMC@users.noreply.github.com>
2023-12-01 00:12:39 +0300

    first commit

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+def island_perimeter(grid):
+    if not grid or not grid[0]:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Each land cell contributes 4 sides to the perimeter
+
+                # Check neighboring cells
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Subtract 2 for each shared side with the upper cell
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Subtract 2 for each shared side with the left cell
+
+    return perimeter
